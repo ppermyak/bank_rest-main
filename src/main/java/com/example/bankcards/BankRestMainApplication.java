d547fcdc3b9c8ce2074f193568bb0052ba7665d5
Айаал <aiaal.permyakov@mail.ru>
2026-05-12 19:12:50 +0300

    Добавил главный класс приложения - BankRestMainApplication

---
+++ src/main/java/com/example/bankcards/BankRestMainApplication.java
@@ -0,0 +1,12 @@
+package com.example.bankcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRestMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankRestMainApplication.class, args);
+    }
+}
